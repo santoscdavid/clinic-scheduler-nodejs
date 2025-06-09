74bd61ffa471181cb268aa3b8548a65f876fa707
santoscdavid <dev.davidcsantos@gmail.com>
2025-06-09 18:22:46 -0300

    feat(appointments): enhance appointment retrieval with doctor and patient relations

--- src/appointments/entities/appointment.entity.ts
+++ src/appointments/entities/appointment.entity.ts
@@ -1,12 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { Doctor } from '../../doctors/entities/doctor.entity';
+import { User } from '../../users/entities/user.entity';
 
 @Entity()
 export class Appointment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ManyToOne(() => User, { nullable: true })
+  @JoinColumn({ name: 'patientId' })
+  patient: User;
+
   @Column({ nullable: true })
   patientId: string;
+
+  @ManyToOne(() => Doctor)
+  @JoinColumn({ name: 'doctorId' })
+  doctor: Doctor;
 
   @Column()
   doctorId: string;
